128a237ccc5d67efe3b304047eeded25a32818a1
bulat <b.barzhanov@yandex.ru>
2021-07-16 18:38:09 +0300

    update get_fork and notification

--- philo/groundhog_day.c
+++ philo/groundhog_day.c
@@ -3,7 +3,7 @@
 void	philo_eat(t_philo *philo)
 {
 	pthread_mutex_lock(&g_chat);
-	printf("\x1b[32m%zu %d is eating\n",
+	printf("%s⏰%zu\t%d\t🍔is eating🍟\n", GREEN,
 		get_time() - philo->start_time, philo->philo_num + 1);
 	philo->start_life = get_time();
 	pthread_mutex_unlock(&g_chat);
@@ -13,60 +13,37 @@ void	philo_eat(t_philo *philo)
 void	philo_sleep(t_philo *philo)
 {
 	pthread_mutex_lock(&g_chat);
-	printf("%zu %d is sleeping\n",
+	printf("%s⏰%zu\t%d\t💤is sleeping💤\n", BLUE,
 		get_time() - philo->start_time, philo->philo_num + 1);
 	pthread_mutex_unlock(&g_chat);
 	my_usleep(philo->time_to_sleep);
+	philo_think(philo);
 }
 
 void	philo_think(t_philo *philo)
 {
 	pthread_mutex_lock(&g_chat);
-	printf("\x1b[35m%zu %d is thinking\n",
+	printf("%s⏰%zu\t%d\t🎻is thinking🎻\n", YELLOW,
 		get_time() - philo->start_time, philo->philo_num + 1);
 	pthread_mutex_unlock(&g_chat);
 }
 
-void	get_fork(t_philo *philo)
+void	get_fork(t_philo *philo, mutex_t *first, mutex_t *second)
 {
-	if (philo->philo_num % 2 == 0)
-	{
-		pthread_mutex_lock(philo->right);
-		pthread_mutex_lock(&g_chat);
-		printf("%zu %d has taken a right fork\n",
-			get_time() - philo->start_time, philo->philo_num + 1);
-		pthread_mutex_unlock(&g_chat);
-		pthread_mutex_lock(&philo->left);
-		pthread_mutex_lock(&g_chat);
-		printf("%zu %d has taken a left fork\n",
-			get_time() - philo->start_time, philo->philo_num + 1);
-		pthread_mutex_unlock(&g_chat);
-	}
-	else
-	{
-		pthread_mutex_lock(&philo->left);
-		pthread_mutex_lock(&g_chat);
-		printf("%zu %d has taken a left fork\n",
-			get_time() - philo->start_time, philo->philo_num + 1);
-		pthread_mutex_unlock(&g_chat);
-		pthread_mutex_lock(philo->right);
-		pthread_mutex_lock(&g_chat);
-		printf("%zu %d has taken a right fork\n",
-			get_time() - philo->start_time, philo->philo_num + 1);
-		pthread_mutex_unlock(&g_chat);
-	}
+	pthread_mutex_lock(first);
+	pthread_mutex_lock(&g_chat);
+	printf("%s⏰%zu\t%d\t🥄has taken a fork🥄\n", WHITE,
+		get_time() - philo->start_time, philo->philo_num + 1);
+	pthread_mutex_unlock(&g_chat);
+	pthread_mutex_lock(second);
+	pthread_mutex_lock(&g_chat);
+	printf("%s⏰%zu\t%d\t🥄has taken a fork🥄\n", WHITE,
+		get_time() - philo->start_time, philo->philo_num + 1);
+	pthread_mutex_unlock(&g_chat);
 }
 
-void	throw_fork(t_philo *philo)
+void	throw_fork(t_philo *philo, mutex_t *first, mutex_t *second)
 {
-	if (philo->philo_num % 2 == 0)
-	{
-		pthread_mutex_unlock(philo->right);
-		pthread_mutex_unlock(&philo->left);
-	}
-	else
-	{
-		pthread_mutex_unlock(&philo->left);
-		pthread_mutex_unlock(philo->right);
-	}
+	pthread_mutex_unlock(first);
+	pthread_mutex_unlock(second);
 }
